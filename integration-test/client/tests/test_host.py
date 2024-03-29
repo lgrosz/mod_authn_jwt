1accc96c538ef2fa09084959474ea07ba328d8d8
Logan Grosz <logan.grosz@gmail.com>
2024-03-28 23:49:16 -0600

    Removes extraneous trailing space

--- integration-test/client/tests/test_host.py
+++ integration-test/client/tests/test_host.py
@@ -6,6 +6,6 @@ from hosts import LIGHTTPD
 
 def test_host_reachability():
     response_time = ping(LIGHTTPD)
-    
+
     # Assert that the response time is not None (i.e., host is reachable)
     assert response_time is not False, f"Host {LIGHTTPD} is unreachable"
