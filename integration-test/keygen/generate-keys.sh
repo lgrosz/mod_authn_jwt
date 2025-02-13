4320b3ceb18321942e636e8da390f7d1e8341e70
Logan.Grosz <logan.grosz@gmail.com>
2025-02-13 12:33:41 -0700

    refactor!: use libjwt-3.1.x
    This is more or less just a port of libjwt-1 to libjwt-3.. that is this
    was mostly `s/jwt_valid/jwt_checker/g`
    
    Some of the slightly more involved bits include
    
    - setting the remote-user is done between token parsing and verification
      through the new libjwt-3 callback paradigm.. this is also where we can
      implement arbitrary checking
    
    - setup checker key during configuration, I would guess this speeds up
      the checking a bit

--- integration-test/keygen/generate-keys.sh
+++ integration-test/keygen/generate-keys.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 openssl genpkey -algorithm RSA -out /private/private.pem > /dev/null 2>&1
+key2jwk -o /private/jwk.json /private/private.pem
+
 openssl rsa -pubout -in /private/private.pem -out /public/public.pem > /dev/null 2>&1
+key2jwk -o /public/jwk.json /public/public.pem
